b32b5ac7b923c0a443ffac3f100990111833bfa2
Chandan Kumar Singh <chandankusingh4545@gmail.com>
2022-12-25 17:36:33 +0530

    mongoose.set('strictQuery', false);

--- server.js
+++ server.js
@@ -4,38 +4,36 @@ const uri =
   "mongodb+srv://tds:rrr123@node-rest-app.9jvqlbd.mongodb.net/Creator_App";
 // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-const bodyParser=require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-mongoose.set('strictQuery', false);
+
 const app = express();
+mongoose.set("strictQuery", false);
 mongoose.connect(uri).then(() => {
- try{ console.log("Connected to db ...");
+  try {
+    console.log("Connected to db ...");
+    const bodyParser = require("body-parser");
 
- 
-
-
-  app.get("/", (req, res, next) => {
-    res.send({
-      status: res.statusCode,
-      message: "Hello people , welcome to My node api",
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.get("/", (req, res, next) => {
+      res.send({
+        status: res.statusCode,
+        message: "Hello people , welcome to My node api",
+      });
     });
-  });
 
-
-  ///User Apis
-  const userApis=require('./apis/userApis');
-  const notesApis=require('./apis/notesApis');
-  const taskApis=require('./apis/taskApis');
-  const collabsApis=require('./apis/collab');
-  const responsedTaskApis=require('./apis/responsedTask');
-  app.use('/user',userApis);
-  app.use('/notes',notesApis);
-  app.use('/tasks',taskApis);
-  app.use('/collabs',collabsApis);
-  app.use('/responsedTask',responsedTaskApis);}
-  catch(err){
+    ///User Apis
+    const userApis = require("./apis/userApis");
+    const notesApis = require("./apis/notesApis");
+    const taskApis = require("./apis/taskApis");
+    const collabsApis = require("./apis/collab");
+    const responsedTaskApis = require("./apis/responsedTask");
+    app.use("/user", userApis);
+    app.use("/notes", notesApis);
+    app.use("/tasks", taskApis);
+    app.use("/collabs", collabsApis);
+    app.use("/responsedTask", responsedTaskApis);
+  } catch (err) {
     console.log(`some thing went wrong ${err}`);
   }
 });
@@ -45,4 +43,4 @@ app.listen(port, () => {
   console.log(`App listening at port ${port}`);
 });
 
-module.exports=app;
+module.exports = app;
